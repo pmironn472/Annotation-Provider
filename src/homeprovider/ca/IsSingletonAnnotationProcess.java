b92a37cb0ec9a89050118c96c773b5d5270be9a4
pmironn472 <63416798+pmironn472@users.noreply.github.com>
2020-09-10 12:56:08 +0300

    Add files via upload

---
+++ src/homeprovider/ca/IsSingletonAnnotationProcess.java
@@ -0,0 +1,44 @@
+package homeprovider.ca;
+
+import java.util.Set;
+
+import javax.annotation.processing.AbstractProcessor;
+import javax.annotation.processing.Messager;
+import javax.annotation.processing.RoundEnvironment;
+import javax.annotation.processing.SupportedAnnotationTypes;
+import javax.lang.model.element.Element;
+import javax.lang.model.element.ElementKind;
+import javax.lang.model.element.TypeElement;
+import javax.tools.Diagnostic.Kind;
+
+
+@SupportedAnnotationTypes(value = {"homeprovider.ca.IsSingleton"})
+public class IsSingletonAnnotationProcess  extends AbstractProcessor{
+	
+	 @Override
+	    public boolean process(Set<? extends TypeElement> annotations,
+	            RoundEnvironment roundEnv) {
+
+	        TypeElement IsSingleton = processingEnv.getElementUtils().getTypeElement("homeprovider.ca.IsSingleton");
+	        Set<? extends Element> classes = roundEnv.getElementsAnnotatedWith(IsSingleton);
+	        Messager messenger = processingEnv.getMessager();
+	        for (Element e : classes) {
+	            boolean found = false;
+	            for (Element method : e.getEnclosedElements()) {
+	                messenger.printMessage(Kind.ERROR, 
+	                        method.getSimpleName());
+	                if (method.getKind() == ElementKind.METHOD && method.getSimpleName().toString().equals("getInstance")) {
+	                    found = true;
+	                    break;
+	                }
+	            }
+	            if (!found)
+	            {
+	                messenger.printMessage(Kind.ERROR, 
+	                    "The following class does not implement getInstance : " + e.getSimpleName(),e);
+	            }
+	        }
+	        return true;
+	    }
+
+}
